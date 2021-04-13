7bdd5525c983c0c24825b4823feb207a62bbc9f0
Adam-smh <africa.adam1@gmail.com>
2021-04-13 09:36:14 +0200

    Added main.py file

---
+++ main.py
@@ -0,0 +1,10 @@
+# Task 2 Sample Solution
+Radius= input("Please Enter Radius:")
+_PI=3.14
+
+Area=_PI * int(Radius)**2
+C= 2 * _PI * int(Radius)
+
+print("Area of Circle is:" + str(Area))
+print("Circumference of Circle is:" + str(C))
+
